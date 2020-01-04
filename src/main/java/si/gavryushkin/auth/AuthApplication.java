f2280ed7eba0bc4cde33f0c38460be776c168489
Гаврюшкин Станислав Игоревич <Gavryushkin.S.I@sberbank.ru>
2020-01-04 17:36:37 +0300

    AUTH-1

--- src/main/java/si/gavryushkin/auth/AuthApplication.java
+++ src/main/java/si/gavryushkin/auth/AuthApplication.java
@@ -10,16 +10,22 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.time.LocalDate;
 import java.util.UUID;
+
 
 @SpringBootApplication
 public class AuthApplication implements CommandLineRunner {
+    JFrame frame=null;
     JLabel label1 = new JLabel("ФИО:");
     JTextField field1 = new JTextField();
     JLabel label2 = new JLabel("Телефон:");
     JTextField field2 = new JTextField();
     JLabel label3 = new JLabel("E-mail:");
     JTextField field3 = new JTextField();
+    JComboBox comboBox = null;
 
     @Autowired
     JdbcOperations jdbcOperations;
@@ -30,16 +36,16 @@ public class AuthApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        JFrame frame = new JFrame("Authorization v.1.0");
+        frame = new JFrame("Authorization v.1.0");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 200);
         JPanel panel = new JPanel((new BorderLayout()));
         JLabel label = new JLabel("Тип подписки:");
         String[] typeTariff = {
-                "Месячная подписка",
-                "Подписка на год",
+                "Monthly Subscription",
+                "Annual Subscription",
         };
-        JComboBox comboBox = new JComboBox(typeTariff);
+        comboBox = new JComboBox(typeTariff);
         ActionListener actionListener1 = new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 e.getSource();
@@ -76,15 +82,36 @@ public class AuthApplication implements CommandLineRunner {
 
     private String insertInDB() {
         String uuid = UUID.randomUUID().toString();
+        Connection con = null;
+        PreparedStatement preparedStatement = null;
+        LocalDate date = LocalDate.now();
+        java.sql.Date sqlDate = java.sql.Date.valueOf(date.plusMonths(1));
+        final String ADD_QUERY;
+        try {
+            ADD_QUERY = "insert into AUTH (UUID,BLOCKED,ACTIVATE_DATE,DEACTIVATE_DATE,FIO,PHONE,MAIL,TARIFF)"
+                    + " values (?,0,CURRENT_DATE,?,?,?,?,?)";
 
-        final String ADD_QUERY = "insert into AUTH (UUID,ACTIVATE_DATE,DEACTIVATE_DATE)\n"
-                + "values (" + uuid + " ,CURRENT_DATE,CURRENT_DATE+30)";
+            con = new SSHConnection().openSSH();
+            preparedStatement = con.prepareStatement(ADD_QUERY);
+            preparedStatement.setString(1, uuid);
+            preparedStatement.setObject(2, sqlDate);
+            preparedStatement.setString(3, field1.getText());
+            preparedStatement.setString(4, field2.getText());
+            preparedStatement.setString(5, field3.getText());
+            preparedStatement.setObject(6, comboBox.getSelectedItem());
+            preparedStatement.executeUpdate();
 
-        final String SELECT_QUERY = "select UUID from AUTH";
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(frame, e.getMessage());
+        } finally {
+            try {
+                con.close();
+                preparedStatement.close();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(frame, "Соединение не закрывается");
+            }
+        }
 
-        jdbcOperations.execute(ADD_QUERY);
-
-
-        return "Запись добавлена!";
+        return uuid;
     }
 }
